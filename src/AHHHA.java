6bdb07adc695358e6b5545ed61686c095a016674
Mudit <muditsekhri@gmail.com>
2022-03-31 16:23:21 +0530

    changes made

---
+++ src/AHHHA.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+public class AHHHA {
+   public static void main(String []args) {
+       int r = 5;
+       int d = 12;
+               do {
+                   System.out.println("" + r++ );
+                   d++;
+                                  }
+       while (d <= 999 );
+
+
+
+
+
+
+
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
